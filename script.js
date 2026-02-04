3f5f325fbdaf732507fa7919ba3ebeb7704c1184
SolomonOzor <157052930+SolomonOzorSectionE@users.noreply.github.com>
2026-02-04 10:19:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+var studentName = "Solomon";
+var studentAge = "22";
+let isEnrolled = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+
+
+let textValue = "Hola Senor";
+console.log(textValue);
+
+let number  = "67";
+console.log(number);
+
+let boolean = true;
+console.log(boolean);
+
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits);
+
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: "67"
+}
+console.log(person);
+
+
+
+
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+
+
+let x = 5; 
+let y = 10;
+console.log("10" == y);
+console.log("10" === 3);
+console.log(x != y);
+console.log(x < y);
+console.log(x > y);
+
+
+
+let c = true;
+let d = false;
+console.log(c && d);
+console.log(c || d);
+console.log(!c);
